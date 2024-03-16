53d985ac8fd18beafc8fc5a9b62be6633610f356
Thomas Heimberger <theimberger@gmail.com>
2024-03-16 11:50:10 -0400

    Update manifest

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
     manifest: {
+      "name": "Record Needles - a simple stitch counter",
       "short_name": "Record Needles",
-      "name": "Record Needles: a Simple Stitch Counter",
-      "icons": [],
-      "background_color": "#3367D6",
+      "start_url": ".",
       "display": "standalone",
-      "scope": "/",
-      "theme_color": "#3367D6",
-      "shortcuts": [],
-      "description": "A stitch counter",
-      "screenshots": []
+      "background_color": "#fff",
+      "description": "A stitch counting app.",
+      "icons": [],
+      "related_applications": []
     }
   })],
   base: '/record-needles/'
