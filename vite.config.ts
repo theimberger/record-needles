c41600dfbf0270d4d0041bc2b27b78606deda080
Thomas Heimberger <theimberger@gmail.com>
2024-03-16 11:40:35 -0400

    Add plugin for pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    manifest: {
+      "short_name": "Record Needles",
+      "name": "Record Needles: a Simple Stitch Counter",
+      "icons": [],
+      "background_color": "#3367D6",
+      "display": "standalone",
+      "scope": "/",
+      "theme_color": "#3367D6",
+      "shortcuts": [],
+      "description": "A stitch counter",
+      "screenshots": []
+    }
+  })],
   base: '/record-needles/'
 })
